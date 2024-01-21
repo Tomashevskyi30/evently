8123e8a04c80d0d7beb70499b0c9cfc3a9f29a87
Tomashevskyi30 <tomashevskyi30@gmail.com>
2024-01-21 16:30:03 +0200

    fix clerk

--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,19 +1,19 @@
-import { Schema, model, models, Document } from "mongoose";
+import { Document, Schema, model, models } from "mongoose";
 
 export interface IEvent extends Document {
   _id: string;
   title: string;
   description?: string;
   location?: string;
-  createdAt?: Date;
+  createdAt: Date;
   imageUrl: string;
-  startDateTime?: Date;
-  endDateTime?: Date;
-  price?: string;
-  isFree?: boolean;
+  startDateTime: Date;
+  endDateTime: Date;
+  price: string;
+  isFree: boolean;
   url?: string;
-  category?: { _id: string; name: string };
-  organizer?: { _id: string; firstName: string; lastName: string };
+  category: { _id: string, name: string }
+  organizer: { _id: string, firstName: string, lastName: string }
 }
 
 const EventSchema = new Schema({
@@ -27,10 +27,10 @@ const EventSchema = new Schema({
   price: { type: String },
   isFree: { type: Boolean, default: false },
   url: { type: String },
-  category: { type: Schema.Types.ObjectId, ref: "Category" },
-  organizer: { type: Schema.Types.ObjectId, ref: "User" },
-});
+  category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  organizer: { type: Schema.Types.ObjectId, ref: 'User' },
+})
 
-const Event = models.Event || model("Event", EventSchema);
+const Event = models.Event || model('Event', EventSchema);
 
 export default Event;
