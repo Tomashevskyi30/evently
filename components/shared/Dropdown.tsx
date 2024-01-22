2d674a5b5c726cfe0d6ed4368cce8cc41ab0341c
Tomashevskyi30 <tomashevskyi30@gmail.com>
2024-01-22 23:42:23 +0200

    collection implementation fix

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -16,7 +16,7 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
-import { ICategory } from "@/lib/database/models/category.modal";
+import { ICategory } from "@/lib/database/models/category.model";
 import { startTransition, useEffect, useState } from "react";
 import { Input } from "../ui/input";
 import {
